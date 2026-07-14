94d7a08b3087b5055fe46c401b90a7f33f0145c2
Lukáš Hozda <luk.hozda@gmail.com>
2026-07-14 23:35:03 +0200

    Export symbols

--- source/package.lisp
+++ source/package.lisp
@@ -74,7 +74,8 @@
            #:config-directory
            #:quicklisp-setup
            #:shell-error
-           #:command-not-found-error)
+           #:command-not-found-error
+           #:pipeline-syntax-error)
   (:documentation "A system shell running inside Clozure CL."))
 
 (defpackage #:cclsh-user
